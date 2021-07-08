82fec6f2936ae99a32395e8fd710c593a203abea
Sakan Komolvatin <sakan.k@kbtg.tech>
2021-07-08 23:23:22 +0700

    Phase 1

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -1,4 +1,4 @@
-export function generatePieChart() {
+export async function generatePieChart() {
   // Set new default font family and font color to mimic Bootstrap's default styling
   (Chart.defaults.global.defaultFontFamily = "Nunito"),
     '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
